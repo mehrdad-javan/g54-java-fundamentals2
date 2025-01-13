03c3c0ce2ac2cc16fecd81d251a6ffbee13dd5b7
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-01-13 10:17:51 +0100

    Overloading & String Class

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,7 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(MathOperations.add(10, 10));
+        PaymentProcessor.processPayment("1234-5678-9012-3456", "123", 150.0);
     }
 }
